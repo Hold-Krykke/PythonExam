aa264c6b3c4857ae13d58a12f9c614036f7d8358
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-04 10:26:33 +0200

    Imports

--- modules/Presentation/presentation.py
+++ modules/Presentation/presentation.py
@@ -1,3 +1,9 @@
 # Presentation file
 # Contains charts
+# Start-point is this tutorial: https://stackabuse.com/python-for-nlp-sentiment-analysis-with-scikit-learn//
+# IMPORTS:
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import re
+import nltk
