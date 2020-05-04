a302a424cd4fd45c21ad5d2e8b799a67082ad018
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-05-04 10:42:47 +0200

    Importing test_data

--- modules/Presentation/presentation.py
+++ modules/Presentation/presentation.py
@@ -7,3 +7,29 @@ import numpy as np
 import pandas as pd
 import re
 import nltk
+
+# Test Data Path
+test_data_path = "../../data/test_tweets/presentation_test_tweets.csv"
+
+
+def get_data_from_csv(test_data_path):
+    """
+    Importing CSV data as pandas DataFrame. 
+
+    Parameters:
+    test_data_path (string): Path to the CSV data you want to import. 
+
+    Returns:
+    Pandas DataFrame
+    """
+
+    test_tweets = pd.read_csv(test_data_path)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
+    return test_tweets
+
+
+# Getting the test_data.
+test_data = get_data_from_csv(test_data_path)
+# Manually testing if we got it how we want it.
+print(test_data.head())
