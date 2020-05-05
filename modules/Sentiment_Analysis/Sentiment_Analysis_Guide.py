6e16dd96eca4aaa89330953b40d318bcf75798e6
Camilla Staunstrup <camilla.valerius@gmail.com>
2020-05-05 16:40:20 +0200

    more cleaning

--- modules/Sentiment_Analysis/Sentiment_Analysis_Guide.py
+++ modules/Sentiment_Analysis/Sentiment_Analysis_Guide.py
@@ -13,32 +13,12 @@ from nltk.tokenize import word_tokenize
 from ala_Runi_tweets import test_tweets
 
 
-# Basic Preprocessing, will be replaced by Rúnis work - Tokenizing the Data
-positive_tweets = twitter_samples.strings('positive_tweets.json')
-negative_tweets = twitter_samples.strings('negative_tweets.json')
-text = twitter_samples.strings('tweets.20150430-223406.json')
-
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-# print(tweet_tokens[0])
-
-# Basic Preprocessing, will be replaced by Rúnis work - Normalizing the Data
+# positive_tweets = twitter_samples.strings('positive_tweets.json')
+# negative_tweets = twitter_samples.strings('negative_tweets.json')
+# text = twitter_samples.strings('tweets.20150430-223406.json')
 
 
-def lemmatize_sentence(tokens):
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_sentence = []
-    for word, tag in pos_tag(tokens):
-        if tag.startswith('NN'):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
-    return lemmatized_sentence
-
-
-# Basic Preprocessing, will be replaced by Rúnis work - Removing Noise from the Data
+####### Basic Preprocessing, will be replaced by Rúnis work #######
 def remove_noise(tweet_tokens, stop_words=()):
 
     cleaned_tokens = []
@@ -63,6 +43,26 @@ def remove_noise(tweet_tokens, stop_words=()):
     return cleaned_tokens
 
 
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+####### Basic Preprocessing, will be replaced by Rúnis work #######
+
+
+################### Test Tweets for building ####################
+positive_tweets = twitter_samples.strings('positive_tweets.json')
+negative_tweets = twitter_samples.strings('negative_tweets.json')
+text = twitter_samples.strings('tweets.20150430-223406.json')
+################### Test Tweets for building ####################
+
+
 stop_words = stopwords.words('english')
 
 positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
@@ -77,31 +77,20 @@ for tokens in positive_tweet_tokens:
 for tokens in negative_tweet_tokens:
     negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-
-# Basic Preprocessing, will be replaced by Rúnis work - Determining Word Density
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
-
-
 all_pos_words = get_all_words(positive_cleaned_tokens_list)
+
 freq_dist_pos = FreqDist(all_pos_words)
-
-
-# Preparing Data and Building and Testing the Model - should be redone once Rúnis part is done
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
-
+# print("Freq")
+# print(freq_dist_pos.most_common(10))
 
 positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
 negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
 
+positive_dataset = [(tweet_dict, "Positive")
+                    for tweet_dict in positive_tokens_for_model]
 
-positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model]
-
-negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model]
+negative_dataset = [(tweet_dict, "Negative")
+                    for tweet_dict in negative_tokens_for_model]
 
 dataset = positive_dataset + negative_dataset
 
@@ -111,6 +100,8 @@ train_data = dataset[:7000]
 test_data = dataset[7000:]
 print('TEST DATA SET', test_data[0])
 
+
+###
 classifier = NaiveBayesClassifier.train(train_data)
 
 # print("Accuracy is:", classify.accuracy(classifier, test_data))
