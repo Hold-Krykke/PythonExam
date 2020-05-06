cb90bb9632b3ab278aa24731adedf36ee9228fd0
Runi <Runiniclasen@hotmail.com>
2020-05-06 17:47:12 +0200

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#import nltk
+
+# will take some time
+# by default it downloads to appData\roaming\nltk_data
+
+# nltk.download('punkt') #pre-trained model for tokenizing, good for punctuations. http://www.nltk.org/api/nltk.tokenize.html?highlight=punkt
+# nltk.download('wordnet') #Lexical database used to determine base words. (stemming) https://www.nltk.org/api/nltk.corpus.reader.html#module-nltk.corpus.reader.wordnet
+# nltk.download('averaged_perceptron_tagger') # Used in conjunction with others to tag words of their part of speech (noun, verb, etc)
+# nltk.download('stopwords') # Used in conjunction with others to remove filler words. https://www.nltk.org/book/ch02.html#wordlist-corpora
