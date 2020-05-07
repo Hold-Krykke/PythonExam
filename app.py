ae8e21d79ce54f5dd674ca3eadfae1f2fb8febbd
Runi <Runiniclasen@hotmail.com>
2020-05-07 17:47:56 +0200

    humble beginnings of app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("Python")
+from modules import Preprocessing
+
+# scrape here
+
+# open file for preprocessing
+FILENAME = './'
+with open(FILENAME) as file_object:
+    tweet_data = file_object.read()
+
+tweets = Preprocessing.get_tweet_data(tweet_data)
+
+# analysis here
+
+# presentation here
+
+
+if __name__ == "__main__":
+    pass
