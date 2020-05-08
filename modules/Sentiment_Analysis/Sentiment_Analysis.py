09414c9f6dae253b53d2157ed148d20099011ad8
Camilla Staunstrup <camilla.valerius@gmail.com>
2020-05-08 13:45:17 +0200

    Commit is for testing - contains testdata and Rúnis preprocessing code

--- modules/Sentiment_Analysis/Sentiment_Analysis.py
+++ modules/Sentiment_Analysis/Sentiment_Analysis.py
@@ -9,18 +9,48 @@ import random
 from nltk import classify
 from nltk import NaiveBayesClassifier
 from nltk.tokenize import word_tokenize
-from ala_Runi_tweets import test_tweets
+from ala_Runi_tweets import test_tweets  # should be removed when merged
+from ala_scraped import scraped_tweets  # should be removed when merged
+
+# These are the only methods that should be called from other modules:
+# train_model_if_necessary()
+# analyze_many_tweets(scraped_tweets, 0.25, 0.75)
 
 
-###### Preprocessing util methods will be replaced by Rúni ######
-def remove_noise(tweet_tokens, stop_words=()):
+########################################################################################################################################################################
+from datetime import datetime
+from typing import List, Dict
 
+
+_stop_words = stopwords.words('english')
+_stop_words.extend(['twitter', 'nt'])
+
+
+def _handle_date(date_string: str):
+    """
+    We receive dates from tweets in the format 'yyyy-m-d'.
+    This function returns a datetime.date object with proper formatting (yyyy-mm-dd)
+    """
+    # return date(*[int(date) for date in date_string.split(',')]) # sorry we didnt get to use you ;(
+    return datetime.strptime(date_string, '%Y,%m,%d').date()
+
+
+def _remove_noise(tweet: str):
+    """
+    Removes noise from the tweets by:
+    Tokenizing (Splits sentences into array of words)
+    Removes hyperlinks with regex
+    Removes special characters (primarily used for emojis) as well as numbers.
+    """
     cleaned_tokens = []
-
+    tweet_tokens = word_tokenize(tweet)
     for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
-        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        # remove hyperlinks
+        token = re.sub(
+            '(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)', '', token)
+        # remove special chars, numbers, inc emojies
+        token = re.sub("[^A-Za-z]", "", token)
 
         if tag.startswith("NN"):
             pos = 'n'
@@ -30,103 +60,147 @@ def remove_noise(tweet_tokens, stop_words=()):
             pos = 'a'
 
         lemmatizer = WordNetLemmatizer()
+        # print('tokenb4', token)
         token = lemmatizer.lemmatize(token, pos)
-
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
+        # print('tokenAfter', token)
+        # remove empty tokens, punctuations and stopwords
+        # use substring search (find) instead?
+        token = token.lower().strip()
+        if len(token) > 1 and token not in string.punctuation and token not in _stop_words:
+            cleaned_tokens.append(token)
     return cleaned_tokens
 
 
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
+def get_tweet_data(tweets: List[Dict[str, str]]):
+    """
+    This function takes a list of tweets, containing web scraped dicts (in particular raw_text) and grabs useful information from it.
 
+    As of now it looks for hashtags (#), mentions (@) and emojis.
 
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        print(dict([token, True] for token in tweet_tokens))
-        yield dict([token, True] for token in tweet_tokens)
-###### Preprocessing util methods will be replaced by Rúni ######
+    Following this, it cleans up the data and returns the same object with fields:
+    hashtags, mentions, tweet
+    """
+    # create for-loop on argument "tweets"
+    for tweet in tweets:
+        # prepare format
+        tweet['hashtags'] = tweet.get('hashtags', [])
+        tweet['mentions'] = tweet.get('mentions', [])
+        tweet_text = tweet.get('raw_text')
+        # remove newline characters (necessary to add spaces between words)
+        tweet_text = tweet_text.replace('\n', ' ')
+
+        # check text for hashtags or mentions
+        if (tweet_text != None and '#' or '@' in tweet_text):
+            for word in tweet_text.split(' '):
+                if word.startswith('#'):
+                    tweet['hashtags'].append(word)
+                    tweet_text = tweet_text.replace(word, '')  # remove hashtag
+                if word.startswith('@'):
+                    tweet['mentions'].append(word)
+                    tweet_text = tweet_text.replace(word, '')  # remove mention
+        # handle dates
+        tweet['date'] = _handle_date(tweet['date'])
+        # add emoji descriptions to tweet text
+        if tweet['emojis']:
+            tweet_text += ' '.join(tweet['emojis'])
+        # clear unused words, numbers, symbols and the like
+        tweet['tweet'] = _remove_noise(tweet_text)  # must finish with this
+        print("tweet['tweet']")
+        print(tweet['tweet'])
+    # handle hashtag stats here or in presentation
+    # handle mention stats here or in presentation
+    return tweets
+
+########################################################################################################################################################################
 
 
 ####################### Prepare the Data ########################
-def prepare_training_data_for_model():
-    stop_words = stopwords.words('english')
-
-    positive_tokens = twitter_samples.tokenized('positive_tweets.json')
-    negative_tokens = twitter_samples.tokenized('negative_tweets.json')
+def _prepare_training_data_for_model():
+    """
+    This function gathers the training data sets from nltk corpus twitter_samples, calls the preprocessing functions and then
+    marks the datasets with "Positive" or "Negative", before finally merging and returning them as one dataset. 
+    This is the data used to train our model. 
+    """
+    positive_tweets = twitter_samples.strings('positive_tweets.json')
+    negative_tweets = twitter_samples.strings('negative_tweets.json')
 
     positive_preprocessed_tokens = []
     negative_preprocessed_tokens = []
 
-    for tokens in positive_tokens:
-        positive_preprocessed_tokens.append(remove_noise(tokens, stop_words))
+    for tweet in positive_tweets:
+        positive_preprocessed_tokens.append(_remove_noise(tweet))  # _remove_noise should be called correctly from Rúnis module
 
-    for tokens in negative_tokens:
-        negative_preprocessed_tokens.append(remove_noise(tokens, stop_words))
+    for tweet in negative_tweets:
+        negative_preprocessed_tokens.append(_remove_noise(tweet))  # _remove_noise should be called correctly from Rúnis module
 
-    positive_formatted_tokens = get_tweets_for_model(positive_preprocessed_tokens)
-    negative_formatted_tokens = get_tweets_for_model(negative_preprocessed_tokens)
+    positive_formatted_tokens = _get_tweets_for_model(positive_preprocessed_tokens)
+    negative_formatted_tokens = _get_tweets_for_model(negative_preprocessed_tokens)
 
     positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_formatted_tokens]
     negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_formatted_tokens]
-
     dataset = positive_dataset + negative_dataset
-    # random.shuffle(dataset)
-    # train_data = dataset[:7000]
-    # test_data = dataset[7000:]
 
     return dataset
+
+
+def _get_tweets_for_model(preprocessed_token_list):
+    """
+    Util function that yields one token at a time in this form, which is required by the model: 
+    ({'top': True, 'engage': True, 'member': True, 'community': True, 'week': True}, 'Positive')
+    """
+    for tweet_tokens in preprocessed_token_list:
+        yield dict([token, True] for token in tweet_tokens)
 ####################### Prepare the Data ########################
 
 
 ####################### Analyze the Data ########################
+_training_dataset = _prepare_training_data_for_model()
+_classifier_has_been_trained = False
+_classifier = None
 
-training_dataset = prepare_training_data_for_model()
-classifier_has_been_trained = False
-classifier = None
 
-
-def analyze_many_tweets(tweets_list):
-
-    for item in tweets_list:
+def analyze_many_tweets(tweets_list, uncertain_low: float, uncertain_high: float):
+    """
+    Takes in a list of scraped tweets and calls the analyzer for each, before appending the analyzed 
+    result to the tweet and returning the list
+    """
+    analyzed_tweets = get_tweet_data(tweets_list)
+    for item in analyzed_tweets:
         tweet = item.get("tweet")
-        result = analyze_tweet(tweet)
+        result = _analyze_tweet(tweet, uncertain_low, uncertain_high)
         item["sentiment_analysis"] = [result]
+    return analyzed_tweets
 
-    return tweets_list
 
-
-def analyze_tweet(tweet):
+def _analyze_tweet(tweet, uncertain_low: float, uncertain_high: float):
+    """
+    This function is the analyzer. It trains the model if not already trained (should only happen once per run),
+    classifies the tweet and appends the results to the result-object, which is then returned.
+    """
     train_model_if_necessary()
-    tweet_tokens = remove_noise(word_tokenize(tweet))
-    # Check format
-    # comb = (dict([token, True] for token in tweet_tokens), classifier.classify(dict([token, True] for token in tweet_tokens)))
-    # print("COMB", comb)
-
-    probability_distrubution = classifier.prob_classify(dict([token, True] for token in tweet_tokens))
+    probability_distrubution = _classifier.prob_classify(dict([token, True] for token in tweet))
 
     result = {}
     result["verdict"] = probability_distrubution.max()
     result["positive_procent"] = round(probability_distrubution.prob("Positive"), 2)
     result["negative_procent"] = round(probability_distrubution.prob("Negative"), 2)
-    if (result.get("positive_procent") < 0.75 and result.get("positive_procent") > 0.25):
+    if (result.get("positive_procent") < uncertain_high and result.get("positive_procent") > uncertain_low):
         result["verdict"] = "Uncertain"
 
-    print(result)
-    print()
     return result
 
 
 def train_model_if_necessary():
-    global classifier_has_been_trained
-    global classifier
+    """
+    This function checks if the model has been trained, and trains the model if it hasn't been trained.
+    It should be called from the Main.py on program start, as the training is time-consuming. This should only
+    occur once per run. 
+    """
+    global _classifier_has_been_trained
+    global _classifier
 
-    if (not classifier_has_been_trained):
-        classifier = NaiveBayesClassifier.train(training_dataset)
+    if (not _classifier_has_been_trained):
+        classifier = NaiveBayesClassifier.train(_training_dataset)
         classifier_has_been_trained = True
 
-
-print(analyze_many_tweets(test_tweets))
 ####################### Analyze the Data ########################
---
+++ modules/Sentiment_Analysis/testing_runis.py
@@ -0,0 +1,140 @@
+from datetime import datetime
+import re
+import string
+from typing import List, Dict
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+
+# lazy load stopwords
+_stop_words = stopwords.words('english')
+_stop_words.extend(['twitter', 'nt'])
+
+scraped_tweets = [{'raw_text': '  THANK YOU @LindseyGrahamSC . I know you are #Trump ‘s ally; it’s gratifying to see you stand by #Biden. twitter.com/sarahmucha/sta…\n', 'tweet_urls': ['https://twitter.com/sarahmucha/status/1256404280875114499'], 'emojis': [], 'date': '2020,5,1'},
+                  {'raw_text': '  Tomorrow I’ll be joined by @SebGorka, @STEPHMHAMILL and other distinguished guests to discuss the 2020 Presidential Election. \n\nTune into WJLA 24/7 tomorrow from 10:30-11:30 AM EST for another edition of The Armstrong Williams Show. #AWS #Election2020 #Trump #Biden @ABC7News pic.twitter.com/TTldPAD5RL\n',
+                      'tweet_urls': ['https://twitter.com/Arightside/status/1256364526741729288/video/1'], 'emojis': [], 'date': '2020,5,1'},
+                  {'raw_text': '  The Narc In Chief is eyeing the November Election\n\nCue the nonstop lying and gaslighting through tweets, positive spin TV and his parade of sycophants\n\n@realDonaldTrump is a proven Liar and Divider, and is profoundly Unfit to serve as #POTUS\n#Trump #GoJoe #Biden #TrumpIsAnIdiot pic.twitter.com/SEm270Og9M\n',
+                      'tweet_urls': ['https://twitter.com/rightNtruthMat/status/1257304920039940097/photo/1'], 'emojis': [], 'date': '2020,5,4'},
+                  {'raw_text': '  God bless America . In #trump we trust. Saved the word from #Covid19 single handed.holy moly ...talk about believing your own PR. Can you actually lose #biden\npic.twitter.com/GTRFbuq3f4\n',
+                   'tweet_urls': ['https://twitter.com/realDonaldTrump/status/1257129479455080448/video/1'], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': "  If you're a woman, a person of color, an immigrant, a suburbanite, a senior, college student, college grad, a white male w/a conscience & ANYONE w/a moral center who respects decency, the rule of law & democracy you must vote for @JoeBiden...#Trump #Biden nydailynews.com/opinion/ny-ope…\n",
+                   'tweet_urls': ['https://www.nydailynews.com/opinion/ny-oped-nelson-jacobus-never-trump-20200505-o6w35mfyd5ffvk2jeddgfl4x5i-story.html?fbclid=IwAR1uJj6cyy1yRRcl_8P66Owud-OjHbUy3vW7iO6KsXnSi5TuZeQ6OXyrl0c'], 'emojis': [], 'date': '2020,5,5'},
+                  {'raw_text': '  In the most recent Monmouth Poll...#Biden widens his lead over #trump by 9 points...50-41%.\n\n#RidenWithBiden\n#LetsEndTheNightmare\n#VoteBlue2020 @JoeBiden\n',
+                   'tweet_urls': [], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': ' #Election2020 #ElectoralCollege #Projection\nExpected: #Biden: 352 | #Trump: 186 | +166 EV\n#Biden  Best 412-126  | +286 EV\n#Trump Best: 269-269 Tie\n\nThe tipping point state is North Carolina where Biden is ahead by 4.2%.\n\nelectiongraphs.com/2020ec/\n',
+                   'tweet_urls': ['https://electiongraphs.com/2020ec/'], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': '  RCP Average of 8 #Polls from 5/6-5/24 in 2016:\n#Clinton: 44.1% (Net +1)\n#Trump:  43.1%\n\nRCP Average of 8 #Polls from 4/13-5/5 in 2020:\n#Biden:  47.6% (Net +5.5)\n#Trump: 42.1%\n\nDid 5/6-5/24 for 2016 because that is when Trump was leading in the most polls all year.\n', 'tweet_urls': [
+                  ], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': "  If #Trump grabbed'em by the p*ssy...\n and #Biden slipped a finger in... \nThen #AlexJones should run...\nand he's more likely to win.............. pic.twitter.com/q2RulmpJp7\n",
+                   'tweet_urls': ['https://twitter.com/ElreoCarlos/status/1257762877114470400/video/1'], 'emojis': [], 'date': '2020,5,5'},
+                  {'raw_text': '  .@realDonaldTrump: "I am not fucking losing to @JoeBiden" \n\nOh yes you are, bud. And in a landslide. The biggest, most embarrassing, humiliating, devastating landslide in history. There\'s still time to quit and save orange face... #Trump #Biden huffpost.com/entry/trump-er…\n',
+                   'tweet_urls': ['https://www.huffpost.com/entry/trump-erupt-campaign-team-poll-numbers_n_5eaa44eec5b633a8544528f4'], 'emojis': [], 'date': '2020,4,30'},
+                  {'raw_text': '  🇺🇸#USA, presidential election #poll :\n\n🔼#Biden (D) : 50 % (+2)\n⏬#Trump (R) : 41 % (-3)\n\n#MonmouthUniversity, 04/05/20 pic.twitter.com/L39hSBfg7m\n', 'tweet_urls': [
+                      'https://twitter.com/ElectsWorld/status/1258108199733022720/photo/1'], 'emojis': ['', '', 'UP-POINTING SMALL RED TRIANGLE'], 'date': '2020,5,6'},
+                  {'raw_text': '  Context: DEMOCRAT state & local leaders. They oppose everything Trump says, does, breathes, exists, etc. \n\n#BigMommaYamiche always leaves out that the people attacking #Trump are not attacking him on policy. They hate his guts. So does #NotroiousBMY & other @whca #Biden activists pic.twitter.com/R5E1hmIwad\n',
+                   'tweet_urls': ['https://twitter.com/TYGRRRREXPRESS/status/1258142894839361536/photo/1'], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': '  Stop feeding me with #TRUMP news. It is like a broken record, repeating repeating repeating! Get some new contents up for the poor man. #america #biden #china #northkorea #COVIDー19\n',
+                   'tweet_urls': [], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': '  Dear America: our country is being ravaged by a deadly #pandemic and economic meltdown and THIS is how #Trump spends his time. Is THIS who we want at the controls for another four years? We have a choice. VOTE #BIDEN twitter.com/realDonaldTrum…\n',
+                   'tweet_urls': ['https://twitter.com/realDonaldTrump/status/1256702883199881216'], 'emojis': [], 'date': '2020,5,3'},
+                  {'raw_text': "  The same people who screeched all over Twitter about the #Trump pussy comment are the same people that are now saying they don't care what #Biden did they will vote for him anyway \n\nAbsolute fucking Left wing hypocrites\n",
+                   'tweet_urls': [], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': '  🗳️US Elections Betting Tips🗳️\n\n🗣️Trump Is Struggling In Polls And Needs A Strong Third-Party Challenge\n\n☑️Where is the betting value in the US Elections?\n\nHERE ▶️ bettingtips4you.com/us-election-be…\n#uselection #Trump #Biden pic.twitter.com/4egqsrQAUp\n',
+                   'tweet_urls': ['https://bettingtips4you.com/us-election-betting/', 'https://twitter.com/BTips4you/status/1258037698838880256/photo/1'], 'emojis': ['', '', ''], 'date': '2020,5,6'},
+                  {'raw_text': '  Tara #Reade has changed her story. Now says she NEVER said sexual harassment! #Biden says let her talk and produce any records from the Senate! #Trump has 33 sexual harassment claims, 2 in court and 100’s of NDA’s. @realDonaldTrump let the women speak! twitter.com/summitandnuffi…\n',
+                   'tweet_urls': ['https://twitter.com/summitandnuffin/status/1258137679327346688'], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': '  Monmouth Univ poll  #Trump 41%  #Biden 50%.   Among men Trump leads 44-46, Among women,  Biden leads 56% to 36%. Women hold the purse strings! Not looking good for your rally cry!\n',
+                   'tweet_urls': [], 'emojis': [], 'date': '2020,5,6'},
+                  {'raw_text': ' @haydentiff While 777.77 777 Amash has don\'t some positives follow/follow in foreign policy; overall he scores a D on the SCOPE test. He, #Trump & #Biden will have to bust a— to earn my endorsement & vote! twitter.com/jackhunter74/s…\n',
+                   'tweet_urls': ['https://twitter.com/jackhunter74/status/1258041753493520384'], 'emojis': [], 'date': '2020,5,6'}]
+
+
+def _handle_date(date_string: str):
+    """
+    We receive dates from tweets in the format 'yyyy-m-d'.
+    This function returns a datetime.date object with proper formatting (yyyy-mm-dd)
+    """
+    # return date(*[int(date) for date in date_string.split(',')]) # sorry we didnt get to use you ;(
+    return datetime.strptime(date_string, '%Y,%m,%d').date()
+
+
+def _remove_noise(tweet: str):
+    """
+    Removes noise from the tweets by:
+    Tokenizing (Splits sentences into array of words)
+    Removes hyperlinks with regex
+    Removes special characters (primarily used for emojis) as well as numbers.
+    """
+    cleaned_tokens = []
+    tweet_tokens = word_tokenize(tweet)
+    for token, tag in pos_tag(tweet_tokens):
+
+        # remove hyperlinks
+        token = re.sub(
+            '(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)', '', token)
+        # remove special chars, numbers, inc emojies
+        token = re.sub("[^A-Za-z]", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        # print('tokenb4', token)
+        token = lemmatizer.lemmatize(token, pos)
+        # print('tokenAfter', token)
+        # remove empty tokens, punctuations and stopwords
+        # use substring search (find) instead?
+        token = token.lower().strip()
+        if len(token) > 1 and token not in string.punctuation and token not in _stop_words:
+            cleaned_tokens.append(token)
+    return cleaned_tokens
+
+
+def get_tweet_data(tweets: List[Dict[str, str]]):
+    """
+    This function takes a list of tweets, containing web scraped dicts (in particular raw_text) and grabs useful information from it.
+
+    As of now it looks for hashtags (#), mentions (@) and emojis.
+
+    Following this, it cleans up the data and returns the same object with fields:
+    hashtags, mentions, tweet
+    """
+    # create for-loop on argument "tweets"
+    for tweet in tweets:
+        # prepare format
+        tweet['hashtags'] = tweet.get('hashtags', [])
+        tweet['mentions'] = tweet.get('mentions', [])
+        tweet_text = tweet.get('raw_text')
+        # remove newline characters (necessary to add spaces between words)
+        tweet_text = tweet_text.replace('\n', ' ')
+
+        # check text for hashtags or mentions
+        if (tweet_text != None and '#' or '@' in tweet_text):
+            for word in tweet_text.split(' '):
+                if word.startswith('#'):
+                    tweet['hashtags'].append(word)
+                    tweet_text = tweet_text.replace(word, '')  # remove hashtag
+                if word.startswith('@'):
+                    tweet['mentions'].append(word)
+                    tweet_text = tweet_text.replace(word, '')  # remove mention
+        # handle dates
+        tweet['date'] = _handle_date(tweet['date'])
+        # add emoji descriptions to tweet text
+        if tweet['emojis']:
+            tweet_text += ' '.join(tweet['emojis'])
+        # clear unused words, numbers, symbols and the like
+        tweet['tweet'] = _remove_noise(tweet_text)  # must finish with this
+    # handle hashtag stats here or in presentation
+    # handle mention stats here or in presentation
+    return tweets
+
+
+# new_tweets = get_tweet_data(scraped_tweets)
+
+# for tweet in new_tweets:
+#     print('-----\n', tweet, '\n')
