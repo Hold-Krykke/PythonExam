c3269436da1515ed3661951c516cde45abfbac53
HrBjarup <cph-ab363@cphbusiness.dk>
2020-05-11 11:31:11 +0200

    Fixed some comments

--- modules/web_scraper.py
+++ modules/web_scraper.py
@@ -24,7 +24,7 @@ def _create_tweet_object(tweet_element, hashtags):
     # Creating tweet_urls property which is a list of strings
     tweet_urls = _extract_data_urls(tweet_element)
     tweet_object["tweet_urls"] = tweet_urls
-    # Creating emojis which is a list of strings
+    # Creating emojis property which is a list of strings
     tweet_emoji_descriptions = _emoji_description_extractor(tweet_object["raw_text"])
     tweet_object["emojis"] = tweet_emoji_descriptions
     # Creating the date property
@@ -56,7 +56,7 @@ def _extract_inner_text(tweet_element):
     tweet_text = ""
     for element in tweet_fractions:
         tweet_text += str(element.text)
-    # Create initial tweet object and return it
+    # Return raw text from tweet
     return tweet_text
 
 
@@ -70,17 +70,20 @@ def _extract_data_urls(tweet_element):
     for element in tweet_a_tags:
         try:
             # If the tag has an attribute "data-url" then we take the link from this URL
+            # Only html tags that contain actual links like "www.checkoutthissite.com" will have the "data-url" attribute
             data_url = element["data-url"]
             data_urls.append(data_url)
         except:
+            # Do nothing if we can't find a data-url. 
             pass
+    # Return all found URLs as a list of strings
     return data_urls
 
 
 def _get_tweet_date(tweet_element):
     """
     Returns a datetime object with the date contained in the tweet.\nThe minutes and hours etc. are not accurate.\n
-    If the tweet is from before 2020 it will still be returned as if it was from 2020 :Hide_the_pain_Harold: Who needs tweets from the previous year anyway
+    If the tweet is from before 2020 it will still be returned as if it was from 2020
     """
     # Finding element that contains the timestamp
     timestamp_element = tweet_element.find("td", class_=["timestamp"])
@@ -92,15 +95,15 @@ def _get_tweet_date(tweet_element):
     if "min" in timestamp or timestamp[-1] == "m" or timestamp[-1] == "s" or "h" in timestamp or "now" in timestamp:
         return "{},{},{}".format(datetime.now().date().year, datetime.now().date().month, datetime.now().date().day)
     # Extract date to ensure proper formatting
+    # None of the tweet dates contain a year so we add 2020 to the date to be able to create a proper date object using strptime()
     tweet_date = datetime.strptime(timestamp + " 2020", "%b %d %Y")
-    # The weird looking return statement is made to make it easier for other parts of the program to read the date
+    # The weird looking return statement is made to make it easier for other parts of the program to read the date (2020,5,6)
     return "{},{},{}".format(tweet_date.date().year, tweet_date.date().month, tweet_date.date().day)
 
 
 def _emoji_description_extractor(text: str):
-    """ 58 lol
+    """
     Used to extract the decription of emojis used in the given text.
-    This function only supports 842 emojis. Some descriptions may be empty if the used emoji is rare or whatever
     """
     emoji_list = emoji.emoji_lis(text)
     emoji_descriptions = [str.strip(emoji.demojize(vars.get("emoji")).replace("_", " ").replace(":", "")) for vars in emoji_list]
@@ -111,44 +114,46 @@ def _get_next_page_link(soup: bs4.BeautifulSoup):
     """
     Used to get the link to the next twitter page with older tweets. Extracts the link from given soup (bs4).
     """
-    more_element = soup.find("div", class_=["w-button-more"])
-    a_tag = more_element.find("a")
+    more_tweets_element = soup.find("div", class_=["w-button-more"])
+    a_tag = more_tweets_element.find("a")
     next_page_link = a_tag["href"]
+    # "https://mobile.twitter.com" is not part of the next-page link so we add it our selves
     url_part = "https://mobile.twitter.com"
     return url_part + next_page_link
 
 
 def get_tweets(tweet_count: int, fresh_search: bool, *hashtags: str):
     """
-    --- Returns ---\n
-    List of strings. The contents of each string will be structured as a dictionary
+    #### Returns \n
+    >>> List of strings. The contents of each string will be structured as a dictionary
 
-    --- Parameters ---\n
+    #### Parameters \n
     tweet_count: integer - The amount of tweets you want. Provide a number in an interval of 20.\n
     fresh_search: boolean - Pass True if you want newly downloadet tweets. When passing False the function will return tweets from a file if a file with matching content exists\n
     *hashtags: string - Pass as many strings as you want. Omit the # when passing this parameter.
 
-    --- Wall of Text (Description) ---\n
+    #### Description \n
     This is the MOTHER FUNCTION of this module. Use this function to get tweets.\n
     Omit the # when providing the search parameter. If you want to search for "#trump", provide "trump".\n
     The amount of tweets searched for will always be at least 20 and will be rounded down to the nearest number divisible by 20. 
     Providing 39 will result in 20 tweets. Providing 41 will result in 40 tweets.
     Returns an array of string - each string containing a single tweet.
-    This function saves the tweets using utf-8 encoding which is not very well suited for emojis. 
-    Provide multiple strings as parameters after the tweet_count parameter to search for tweets that contain multiple hashtags.
+    This function saves the tweets in the /tweets folder for faster searching if fresh_search == False 
+    Provide multiple strings as parameters after the fresh_search parameter to search for tweets that contain multiple hashtags.
     Pass True as the fresh_search parameter if you want to make sure you get the newest results from Twitter.
     """
     # Creating initial URL
     URL = _base_URL
     # Name of the file the tweets will be saved in
     file_name = ""
+    # This list is used to add the hashtags searched for to the tweet objects
     hashtag_list = []
     # Creating file name and updating initial URL 
     for index, hashtag in enumerate(hashtags):
         # %23 is URL language for #. Adding + between each search parameter because whitespace is converted to + in the URL
         URL += ("%23" + hashtag + "+")
         hashtag_list.append("#" + hashtag)
-        # If we've reached the last search parameter we end the file name with the last search parameter
+        # If we've reached the last search parameter we end the file name with the last search parameter (the last hashtag)
         if ((len(hashtags) - index) == 1):
             file_name += hashtag
         # If there is more than one search parameter left we add both the current search parameter and an underscore to the file name
@@ -186,7 +191,7 @@ def get_tweets(tweet_count: int, fresh_search: bool, *hashtags: str):
     if not (tweet_count < 20):
         temp_count = tweet_count
     # Creating the number used to choose how many Twitters we need to scrape data from
-    # There are 20 tweets pr page that's why we devide the number of tweets by 20
+    # There are 20 tweets pr page that's why we divide the number of tweets by 20
     loop_count = int(temp_count / 20)
 
     soups = []
@@ -224,15 +229,4 @@ def get_tweets(tweet_count: int, fresh_search: bool, *hashtags: str):
 # Usage example: 20: number of tweets, False: fresh search?, anything after this == search parameters (hashtags)
 # tweets = get_tweets(20, True, "trump")
 # print("Tweets downloaded")
-# for tweet in tweets:
-#     print("\n")
-#     print(str(tweet).encode())
-#     print("\n")
-# print(len(tweets))
-# print(tweets)
-# print("\n")
-# print(len(tweets))
 
-# for tweet in tweets:
-#     desciptions = emoji_description_extractor(tweet)
-#     print(desciptions)
