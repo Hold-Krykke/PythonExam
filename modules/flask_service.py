eef592758059b1e3824d202000720034244db78d
HrBjarup <cph-ab363@cphbusiness.dk>
2020-05-13 17:20:10 +0200

    No one saw this

--- modules/flask_service.py
+++ modules/flask_service.py
@@ -9,7 +9,7 @@ from datetime import datetime, date
 app = Flask(__name__)
 
 @app.route('/api/sentiment', methods=['POST'])
-def get_burglaries():
+def get_sentiment():
     # Check there is a body in request
     if not request.json:
         abort(400, 'Please provide search data. Example: {"hashtags": ["trump","biden"],"start_date": "2020-5-8","end_date": "2020-5-12","plot_type": "line","remove_sentiment": "Uncertain","search_for": {"mentions": "@JoeBiden"},"tweet_count": 300,"fresh_search": false}')
